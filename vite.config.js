fb90f75fa1ba4c2225bfaa243cee7f821f43bf3d
Leo <leo80103@gmail.com>
2024-01-04 18:28:51 +0800

    修改 base 路徑

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "coffee_meets_bagel/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "coffee_meets_bagel/",
 });
