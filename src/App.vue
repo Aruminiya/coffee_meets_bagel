175882aee20905fafb4d805e96476efdf3526b82
Leo <leo80103@gmail.com>
2024-01-23 16:29:37 +0800

    NavBar 元件化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script>
+import NavBarComponent from "./components/NavBarComponent.vue";
+
 export default {
+  components: { NavBarComponent },
   mounted() {
     //顯示當前環境變數 (可以刪掉 不太重要)
     console.log(import.meta.env.VITE_CURRENT_ENVIRONMENT);
@@ -8,34 +11,7 @@ export default {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <router-link to="/" class="navbar-brand" href="#">LOGO</router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <router-link to="/about" class="nav-link" aria-current="page" href="#"
-            >關於我們</router-link
-          >
-        </div>
-        <div class="navbar-nav">
-          <router-link to="/order" class="nav-link" aria-current="page" href="#"
-            >線上點餐</router-link
-          >
-        </div>
-      </div>
-    </div>
-  </nav>
+  <NavBarComponent />
   <h1>這是根組件 App.vue</h1>
   <RouterView />
 </template>
