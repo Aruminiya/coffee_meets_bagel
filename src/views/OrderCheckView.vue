5273e56e7f900512659be707110f159562fbc2c4
Leo <leo80103@gmail.com>
2024-01-23 17:53:13 +0800

    訂單檢查 Bar 切版

---
+++ src/views/OrderCheckView.vue
@@ -0,0 +1,22 @@
+<script>
+import OrderCheckProgress from "../components/OrderCheckProgress.vue";
+
+export default {
+  components: { OrderCheckProgress },
+  data() {
+    return {};
+  },
+  mounted() {
+    const host = import.meta.env.VITE_HEXAPI;
+    const path = import.meta.env.VITE_USER_PATH;
+  },
+};
+</script>
+
+<template>
+  <main>
+    <OrderCheckProgress />
+  </main>
+</template>
+
+<style lang="scss"></style>
