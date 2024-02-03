4e2e693d77a17077509415cf6a7bdc4d7546c247
Leo <leo80103@gmail.com>
2024-02-03 14:28:03 +0800

    新增 Boostrap 可自訂樣式 scss 檔案

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,6 @@ export default {
   <router-link to="/orderCheckView/step1" class="nav-link"
     >訂單查看頁面 (暫時放在這邊)</router-link
   >
-  <button type="button" class="btn btn-primary">Primary</button>
-  <button type="button" class="btn btn-secondary">Secondary</button>
-  <button type="button" class="btn btn-success">Success</button>
-  <button type="button" class="btn btn-danger">Danger</button>
-  <button type="button" class="btn btn-warning">Warning</button>
-  <button type="button" class="btn btn-info">Info</button>
-  <button type="button" class="btn btn-light">Light</button>
-  <button type="button" class="btn btn-dark">Dark</button>
-
-  <button type="button" class="btn btn-link">Link</button>
 </template>
 
 <style lang="scss"></style>
