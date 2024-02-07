84346b53958fc98aed0b3aa189448c154630ace1
Leo <leo80103@gmail.com>
2024-02-07 11:41:10 +0800

    解決小 BUG

--- src/views/frontendViews/productList.vue
+++ src/views/frontendViews/productList.vue
@@ -1,7 +1,7 @@
-<link
+<!-- <link
   rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
-/>
+/> -->
 <script>
 import axios from "axios";
 import VueAxios from "vue-axios";
