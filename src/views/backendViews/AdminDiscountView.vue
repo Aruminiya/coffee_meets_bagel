2dbdbd19bebb1d025c48cecc2434cfe941a8c0f7
Michael Hsia <michaelhsia8716@gmail.com>
2024-02-11 16:17:12 +0800

    切版優化

--- src/views/backendViews/AdminDiscountView.vue
+++ src/views/backendViews/AdminDiscountView.vue
@@ -9,46 +9,52 @@
   <adminNav ref="backendNav" />
   <main>
     <div class="container mt-10 py-5">
-      <div class="row ms-8">
-        <div class="col-md-6 col-lg-4 ps-0">
+      <!-- discount-searchbar -->
+      <div class="row">
+        <div class="col-md-6 col-lg-4">
           <div class="input-group mb-4">
             <input type="text"
               class="form-control fs-3 fw-semibold text-secondary lh-1 border-colorChart-Primary-200 border-end-0 py-3 ps-4"
               placeholder="請輸入搜尋資料" aria-label="Username" aria-describedby="discount-searchbar">
-            <button class="btn btn-colorChart-Primary-200 d-flex align-items-center" type="button"
+            <button class="btn btn-colorChart-Primary-200 d-flex align-items-center px-4" type="button"
               id="discount-searchbar">
-              <span class="material-symbols-outlined cursor-pointer">
+              <span class="material-symbols-outlined cursor-pointer fs-2">
                 search
               </span>
             </button>
           </div>
         </div>
       </div>
-      <h2 class="fs-48 fw-bold text-colorChart-Accessory-200 lh-1 ms-8 mb-4">所有折扣</h2>
-      <div class="border rounded-3 shadow p-4">
-        <table class="table fs-2 align-middle">
+      <!-- 所有折扣、新增折價券 -->
+      <div class="d-flex align-items-center mb-4">
+        <h2 class="fs-48 fw-bold text-colorChart-Accessory-200 lh-1 mb-0 me-9">所有折扣</h2>
+        <button class="btn btn-colorChart-Primary-200 fs-4">新增折價券</button>
+      </div>
+
+      <div class="border rounded shadow p-4 table-responsive">
+        <table class="discountTable table table-light table-striped table-hover table-borderless fs-2 align-middle mb-0">
           <thead>
             <tr>
-              <th>折價券名稱</th>
-              <th>優惠碼</th>
-              <th>折扣額度</th>
-              <th>總數量</th>
-              <th></th>
+              <th width="150" class="top-left-border-radius ps-5 py-9 text-truncate">折價券名稱</th>
+              <th width="150" class="py-9">優惠碼</th>
+              <th width="100" class="py-9 text-truncate">折扣額度</th>
+              <th width="100" class="py-9 text-truncate">總數量</th>
+              <th width="50" class="top-right-border-radius py-9"></th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="coupon in coupons">
-              <td>{{ coupon.title }}</td>
-              <td>{{ coupon.code }}</td>
-              <td>{{ coupon.percent }}%OFF</td>
-              <td>{{ coupon.num }}張
+            <tr v-for="coupon in coupons" :key="coupon.id" class="cursor-pointer">
+              <td class="ps-5 py-11 text-truncate">{{ coupon.title }}</td>
+              <td class="py-11 text-truncate">{{ coupon.code }}</td>
+              <td class="py-11 text-truncate">{{ coupon.percent }}%OFF</td>
+              <td class="py-11 text-truncate">{{ coupon.num }}張
               </td>
-              <td>
-                <div class="d-flex justify-content-between">
-                  <span class="material-symbols-outlined">
+              <td class="py-11 pe-7">
+                <div class="d-flex justify-content-center">
+                  <span class="material-symbols-outlined fs-1 me-6">
                     edit_square
                   </span>
-                  <span class="material-symbols-outlined">
+                  <span class="material-symbols-outlined fs-1">
                     delete
                   </span>
                 </div>
@@ -120,5 +126,31 @@ export default {
 
 .fs-48 {
   font-size: 48px;
+}
+
+.top-left-border-radius {
+  border-radius: 5px 0 0 0;
+}
+
+.top-right-border-radius {
+  border-radius: 0 5px 0 0;
+}
+
+.discountTable {
+  tbody {
+    tr {
+      &:last-child {
+        td {
+          &:first-child {
+            border-radius: 0 0 0 5px;
+          }
+
+          &:last-child {
+            border-radius: 0 0 5px 0;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
