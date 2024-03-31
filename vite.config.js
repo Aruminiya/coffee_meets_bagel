ffe3432d22528ab0fde4543e987170607151eced
DESKTOP-SHGJGCS\leo80 <leo80103@gmail.com>
2024-03-31 20:32:07 +0800

    修正 Base 路徑

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 // eslint-disable-next-line no-control-regex
 const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
 const DRIVE_LETTER_REGEX = /^[a-z]:/i
-console.log(process.env.REPOSITORY_NAME)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
